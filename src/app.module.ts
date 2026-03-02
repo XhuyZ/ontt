374cca81a1e1da51bad785fab1f92cf03b099329
xhuyz <xhuyzdev@gmail.com>
2026-03-03 01:27:57 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,52 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { ProjectModule } from './project/project.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ImageModule } from './image/image.module';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'ontt',
-      autoLoadEntities: true,
-      synchronize: true, // dev only
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        if (process.env.DATABASE_URL) {
+          return {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            autoLoadEntities: true,
+            synchronize: false,
+            logging: false,
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          };
+        }
+
+        // 🛠 Dev fallback
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST || 'localhost',
+          port: Number(process.env.DB_PORT) || 5432,
+          username: process.env.DB_USERNAME || 'postgres',
+          password: process.env.DB_PASSWORD || 'postgres',
+          database: process.env.DB_DATABASE || 'ontt',
+          autoLoadEntities: true,
+          synchronize: true,
+          logging: true,
+          ssl: false,
+        };
+      },
+    }),
+
     ProductModule,
     ProjectModule,
     ImageModule,
@@ -26,3 +55,4 @@ import { ImageModule } from './image/image.module';
   providers: [AppService],
 })
 export class AppModule {}
+
