d85fd1e2d91dbc7c8c144025f49a3721b6da887c
xhuyz <xhuyzdev@gmail.com>
2026-03-05 03:12:13 +0700

    u

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://opnhuatuankiet.io.vn'],
+    origin: ['http://localhost:5173', 'https://api.opnhuatuankiet.io.vn' , 'https://opnhuatuankiet.io.vn'],
     credentials: true,
   });
 
